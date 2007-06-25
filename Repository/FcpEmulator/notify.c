ba2684a01720132eadb7655c58d5477827aa463a
bill <bill>
2007-06-25 07:16:56 +0000

    Fixed Linux diagnostic (added break following conditional compilation within otherwise empty case statement).

--- Repository/FcpEmulator/notify.c
+++ Repository/FcpEmulator/notify.c
@@ -1,4 +1,4 @@
-/* $Header: /home/bill/Repository/FcpEmulator/notify.c,v 1.6 2006/03/23 12:43:21 bill Exp $ */
+/* $Header: /home/bill/Repository/FcpEmulator/notify.c,v 1.7 2007/02/21 16:18:42 bill Exp $ */
 
 #include	<stdio.h>
 extern	FILE *DbgFile, *OutFile;
@@ -1069,6 +1069,7 @@ int signal_event(Sig)
 	      CoreDumped = True;
 	    }
 #endif
+	    break;
 	  }
 
 	  if (!Exiting) {
